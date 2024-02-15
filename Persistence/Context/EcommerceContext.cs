b5d7fd77c39fb9bbe94192e27d726cd4fb687abe
Anderson <andersonrb0330@gmail.com>
2024-02-14 23:31:58 -0500

    proyecto nuevo ncapas

---
+++ Persistence/Context/EcommerceContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Context
+{
+    internal class EcommerceContext : DbContext
+    {
+        public EcommerceContext(DbContextOptions<EcommerceContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<TipoProducto> TipoProductos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Empresa> Empresas { get; set; }
+
+    }
+}
